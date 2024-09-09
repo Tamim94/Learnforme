2367056efe2d2ae6f1bb777b7f09aa26b3aa1b07
tamim94 <golam.tamim94@gmail.com>
2024-09-10 00:18:29 +0200

    Initial commit

---
+++ Learnforme/urls.py
@@ -0,0 +1,40 @@
+"""Learnforme URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/4.1/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
+from django.urls import path
+from django.contrib.auth import views as auth_views
+from . import views
+from .views import index  # Add this line to import the index view
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('', index, name='index'),  # Add this line to set index as the default view
+
+    path('python/', views.python, name='python'),
+    path('javascript/', views.javascript, name='javascript'),
+    path('java/', views.java, name='java'),
+    path('cpp/', views.cpp, name='cpp'),
+    path('php/', views.php, name='php'),
+    path('ruby/', views.ruby, name='ruby'),
+    path('csharp/', views.csharp, name='csharp'),
+    path('login/', views.login, name='login'),
+
+    path('profile/', views.profile, name='profile'),
+    path('logout/', auth_views.LogoutView.as_view(next_page='/'), name='logout'),
+    path('createuser/', views.createuser, name='createuser'),  # Add this line
+
+
+]
---
+++ Learnforme/views.py
@@ -0,0 +1,66 @@
+from django.contrib.auth.models import User
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login as auth_login
+from django.contrib import messages
+from django.shortcuts import render
+
+def index(request):
+    return render(request, 'index.html')
+
+def python(request):
+    return render(request, 'python.html')
+
+def javascript(request):
+    return render(request, 'javascript.html')
+
+def java(request):
+    return render(request, 'java.html')
+
+def cpp(request):
+    return render(request, 'cpp.html')
+
+def php(request):
+    return render(request, 'php.html')
+
+def ruby(request):
+    return render(request, 'ruby.html')
+
+def csharp(request):
+    return render(request, 'csharp.html')
+
+def login(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            auth_login(request, user)
+            return redirect('index')  # Redirect to profile page after successful login
+        else:
+            messages.error(request, 'Nom d\'utilisateur ou mot de passe incorrect.')
+    return render(request, 'login.html')
+
+def createuser(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        email = request.POST['email']
+        password1 = request.POST['password1']
+        password2 = request.POST['password2']
+
+        if password1 == password2:
+            if User.objects.filter(username=username).exists():
+                messages.error(request, 'Nom d\'utilisateur déjà pris.')
+            elif User.objects.filter(email=email).exists():
+                messages.error(request, 'Email déjà utilisé.')
+            else:
+                user = User.objects.create_user(username=username, email=email, password=password1)
+                user.save()
+                messages.success(request, 'Compte créé avec succès.')
+                return redirect('login')
+        else:
+            messages.error(request, 'Les mots de passe ne correspondent pas.')
+    return render(request, 'createuser.html')
+
+def profile(request):
+    return render(request, 'profile.html')
+
